0a338fd255e17dd50e1a348d9d6623dd1df1052d
Arturo Vaine <arturo.vaine@gmail.com>
2022-03-09 17:58:47 -0300

    feat: req9

---
+++ desafio9.sql
@@ -0,0 +1,4 @@
+SELECT COUNT(*) AS quantidade_musicas_no_historico
+FROM SpotifyClone.reproducoes
+LEFT JOIN SpotifyClone.usuarios ON SpotifyClone.usuarios.usuario_id = SpotifyClone.reproducoes.usuario_id
+WHERE SpotifyClone.usuarios.usuario = 'Bill';
