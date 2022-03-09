4e3fbeb69f6713d551699a184244aa3cb00d3cb1
Arturo Vaine <arturo.vaine@gmail.com>
2022-03-09 01:59:23 -0300

    fix: req3

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,6 @@
 SELECT usuario, COUNT(reproducoes.cancao_id) AS qtde_musicas_ouvidas, SUM(duracao_segundos)/60 AS total_minutos
 FROM SpotifyClone.usuarios AS usuarios
-INNER JOIN reproducoes ON usuarios.usuario_id = reproducoes.usuario_id
-INNER JOIN cancoes ON cancoes.cancao_id = reproducoes.cancao_id
+INNER JOIN reproducoes ON SpotifyClone.usuarios.usuario_id = SpotifyClone.reproducoes.usuario_id
+INNER JOIN cancoes ON SpotifyClone.cancoes.cancao_id = SpotifyClone.reproducoes.cancao_id
 GROUP BY usuario
 ORDER BY usuario;
