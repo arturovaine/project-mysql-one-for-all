092ec900f8c2269958f09680ffd3ef62d41f6cab
Arturo Vaine <arturo.vaine@gmail.com>
2022-03-10 20:39:35 -0300

    fix: req3, ROUND(COUNT...,2)

--- desafio3.sql
+++ desafio3.sql
@@ -1,4 +1,4 @@
-SELECT usuario, COUNT(reproducoes.cancao_id) AS qtde_musicas_ouvidas, SUM(duracao_segundos)/60 AS total_minutos
+SELECT usuario, COUNT(reproducoes.cancao_id) AS qtde_musicas_ouvidas, ROUND(SUM(duracao_segundos)/60,2) AS total_minutos
 FROM SpotifyClone.usuarios AS usuarios
 INNER JOIN reproducoes ON SpotifyClone.usuarios.usuario_id = SpotifyClone.reproducoes.usuario_id
 INNER JOIN cancoes ON SpotifyClone.cancoes.cancao_id = SpotifyClone.reproducoes.cancao_id
