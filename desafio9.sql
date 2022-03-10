17a1ab84f8680406351e539eff2db93e11060e5e
Arturo Vaine <arturo.vaine@gmail.com>
2022-03-10 20:45:37 -0300

    fix: req9

--- desafio9.sql
+++ desafio9.sql
@@ -1,4 +1,4 @@
-SELECT COUNT(*) AS quantidade_musicas_no_historico
+SELECT COUNT(SpotifyClone.reproducoes.usuario_id) AS quantidade_musicas_no_historico
 FROM SpotifyClone.reproducoes
 LEFT JOIN SpotifyClone.usuarios ON SpotifyClone.usuarios.usuario_id = SpotifyClone.reproducoes.usuario_id
 WHERE SpotifyClone.usuarios.usuario = 'Bill';
