5595ed823a57993e4043bd610e58ad730a0ae996
Arturo Vaine <arturo.vaine@gmail.com>
2022-03-11 02:28:54 -0300

    refactor: req6

--- desafio6.sql
+++ desafio6.sql
@@ -1,16 +1,7 @@
-SELECT (SELECT ROUND(MIN(SpotifyClone.planos.valor_plano), 2)
-			FROM SpotifyClone.planos
-			LEFT JOIN SpotifyClone.usuarios
-            ON SpotifyClone.usuarios.plano_id = SpotifyClone.planos.plano_id) AS faturamento_minimo,
-		(SELECT ROUND(MAX(SpotifyClone.planos.valor_plano), 2)
-			FROM SpotifyClone.planos
-			LEFT JOIN SpotifyClone.usuarios
-            ON SpotifyClone.usuarios.plano_id = SpotifyClone.planos.plano_id) AS faturamento_maximo,
-		(SELECT ROUND(AVG(SpotifyClone.planos.valor_plano), 2)
-			FROM SpotifyClone.planos
-			LEFT JOIN SpotifyClone.usuarios
-            ON SpotifyClone.usuarios.plano_id = SpotifyClone.planos.plano_id) AS faturamento_medio,
-		(SELECT ROUND(SUM(SpotifyClone.planos.valor_plano), 2)
-			FROM SpotifyClone.planos
-			LEFT JOIN SpotifyClone.usuarios
-            ON SpotifyClone.usuarios.plano_id = SpotifyClone.planos.plano_id) AS faturamento_total;
+SELECT (SELECT ROUND(MIN(SpotifyClone.planos.valor_plano), 2)) AS faturamento_minimo,
+	   (SELECT ROUND(MAX(SpotifyClone.planos.valor_plano), 2)) AS faturamento_maximo,
+	   (SELECT ROUND(AVG(SpotifyClone.planos.valor_plano), 2)) AS faturamento_medio,
+	   (SELECT ROUND(SUM(SpotifyClone.planos.valor_plano), 2)) AS faturamento_total
+FROM SpotifyClone.planos
+LEFT JOIN SpotifyClone.usuarios
+ON SpotifyClone.usuarios.plano_id = SpotifyClone.planos.plano_id;
